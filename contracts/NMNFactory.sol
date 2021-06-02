79c77e43f4b6e3e61c2a9a2460ad30f2e60b1a2a
Naman R Bhardwaj <44472645+naman312010@users.noreply.github.com>
2021-06-02 12:22:10 +0530

    Removed license identifier

--- contracts/NMNFactory.sol
+++ contracts/NMNFactory.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
