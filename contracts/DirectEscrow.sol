c946fc022ac39e543936be3f60e4807de2532f70
Lokesh Kumar Nalot <lokeshkumar@M1-Macbook-Air.local>
2021-06-04 13:13:20 +0530

    escrow update 3

--- contracts/DirectEscrow.sol
+++ contracts/DirectEscrow.sol
@@ -20,8 +20,27 @@ contract escrow is ERC1155Holder, ReentrancyGuard {
     uint256 orderCtr;
     mapping(uint256 => Order) public ORDER;
 
-    // event TokenPurchased(address _seller,address _buyer, uint256 _tokenIdPurchased, uint256 _tokenPurchased);
-    // event SaleCancelled(address _seller);
+    event TokenListed(
+        address _seller,
+        address _contract,
+        uint256 _tokenIdListed,
+        uint256 indexed _amountListed,
+        uint256 _pricePerPiece
+    );
+    event TokenUnlisted(
+        address _seller,
+        address _contract,
+        uint256 indexed _tokenIdUnlisted,
+        uint256 _amountUnlisted
+    );
+    event TokenPurchased(
+        address _seller,
+        address _buyer,
+        address _contract,
+        uint256 indexed _tokenIdPurchased,
+        uint256 _amountBought,
+        uint256 _cost
+    );
 
     function list(
         address _contract,
@@ -57,6 +76,7 @@ contract escrow is ERC1155Holder, ReentrancyGuard {
             _amount,
             ""
         );
+        emit TokenListed(msg.sender, _contract, _tokenId, _amount, _price);
     }
 
     function unlist(uint256 orderID) public {
@@ -69,6 +89,10 @@ contract escrow is ERC1155Holder, ReentrancyGuard {
             "Invalid Order ID provided!!!"
         );
         require(
+            ORDER[orderID].stage != Stage.ORDER_CLOSED,
+            "Order is closed!!!"
+        );
+        require(
             ((ORDER[orderID].stage == Stage.LISTED) ||
                 (ORDER[orderID].stage == Stage.PARTIALLY_SOLD)),
             "Wrong Token stage!!!"
@@ -79,6 +103,12 @@ contract escrow is ERC1155Holder, ReentrancyGuard {
             ORDER[orderID].tokenID,
             ORDER[orderID].amountToSell,
             ""
+        );
+        emit TokenUnlisted(
+            msg.sender,
+            address(ORDER[orderID].contractAddr),
+            ORDER[orderID].tokenID,
+            ORDER[orderID].amountToSell
         );
         ORDER[orderID].amountToSell = 0;
         ORDER[orderID].stage = Stage.ORDER_CLOSED;
@@ -116,9 +146,20 @@ contract escrow is ERC1155Holder, ReentrancyGuard {
             ""
         );
         ORDER[orderID].seller.transfer(cost);
-        ORDER[orderID].amountToSell -= buyAmount;
+        // ORDER[orderID].amountToSell -= buyAmount;
+        ORDER[orderID].amountToSell = ORDER[orderID].amountToSell.sub(
+            buyAmount
+        );
         if (ORDER[orderID].amountToSell == 0)
             ORDER[orderID].stage = Stage.ORDER_CLOSED;
         else ORDER[orderID].stage = Stage.PARTIALLY_SOLD;
+        emit TokenPurchased(
+            ORDER[orderID].seller,
+            msg.sender,
+            address(ORDER[orderID].contractAddr),
+            ORDER[orderID].tokenID,
+            buyAmount,
+            cost
+        );
     }
 }
